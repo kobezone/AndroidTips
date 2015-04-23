fb8bd58e850fde10248af8888d89ffcabae812e6
kobezone <kobezone@163.com>
2015-04-23 11:08:36 +0800

    --Android Tips 项目搭建

---
+++ src/com/learn/android/MainActivity.java
@@ -0,0 +1,21 @@
+package com.learn.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * <pre>
+ *  	这是一个学习和交流的项目,记录个人在android项目开发中的所学习和掌握的技术点,一些个人收集和学习的书籍,心得等,希望和大家一起交流学习.
+ * </pre>
+ * @author JUN 
+ * email jun_dev@qq.com
+ * QQ群 313683173
+ * 
+ */
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
